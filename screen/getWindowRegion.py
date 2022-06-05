21d4e9a048700dd301dc25665bbf88d305a63ce4
Daniel <Cheng_mmm@iCloud.com>
2022-06-06 00:29:18 +0800

    2022/6/6 0:29 彻底解决了抓取屏幕的性能问题，明天还要做更近一步的测试，以便确定没有产生新的bug

--- screen/getWindowRegion.py
+++ screen/getWindowRegion.py
@@ -1,12 +1,21 @@
-import win32gui
+import win32gui, win32print, win32con, win32api
 import sys
+
+def getRate():
+  hwin = win32gui.GetDC(0)
+  # 真实分辨率
+  realX = win32print.GetDeviceCaps(hwin, win32con.DESKTOPHORZRES)
+  # 缩放之后
+  nowX = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
+  rate = realX / nowX
+  return rate
+
 
 def getWindowRegion():
   # WINDOW_TITLE = 'Grand Theft Auto V'
   WINDOW_TITLE = 'test.txt - 记事本'
   window = win32gui.FindWindow(None, WINDOW_TITLE)    
   # 监控的窗口需要运行在窗口状态
-
 
   if not window:
     print('ERROR!!')
@@ -18,12 +27,12 @@ def getWindowRegion():
   # 判断屏幕是否进行了缩放
 
   # 获取的屏幕位置为四个，分别是左上右下
-  pos = (win32gui.GetWindowRect(window))
+  left, up, right, down = (win32gui.GetWindowRect(window))
+  pos = int(left*getRate()), int(up*getRate()), int(right*getRate()), int(down*getRate())
   if (pos[0] or pos[1] or pos[2] or pos[3]) < 0:
     print('ERROR!!\n')
     print('Can not find the window you want\n')
     sys.exit()
-
   return pos
 
 if __name__ == '__main__':
--- screen/grab_screen.py
+++ screen/grab_screen.py
@@ -4,22 +4,8 @@ from screen.getWindowRegion import getWindowRegion
 import win32gui, win32ui, win32con, win32print, win32api
 import numpy as np
 
-def getRate():
-  hwin = win32gui.GetDC(0)
-  # 真实分辨率
-  realX = win32print.GetDeviceCaps(hwin, win32con.DESKTOPHORZRES)
-  # 缩放之后
-  nowX = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
-  rate = realX / nowX
-  return rate
-
 def grabScreen(region):
   left, up, right, down = region
-
-  left = int(left * getRate()) + 10
-  up = int(up * getRate()) + 40
-  right = int(right * getRate()) - 10
-  down = int(down * getRate()) - 10
   width = right -left + 1
   height = down - up + 1
 
