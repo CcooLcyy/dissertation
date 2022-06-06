8000b1d0a1366313f188a8f2602184c64e4b1052
Daniel <Cheng_mmm@iCloud.com>
2022-06-06 08:36:48 +0800

    增加屏幕检测的测试用例，使用opencv显示出来，同时显示的话会降低检测性能

--- 1_collectionDataSets.py
+++ 1_collectionDataSets.py
@@ -1,5 +1,3 @@
-from fileinput import filename
-from turtle import width
 from screen.grab_screen import grabScreen
 from screen.getWindowRegion import getWindowRegion
 import cv2 as cv
@@ -11,43 +9,78 @@ def turnToFPS(time):
   fps = 1 / time
   return fps
 
-START_VALUE = 1
+def main():
+  START_VALUE = 1
 
-trainingData = []
+  trainingData = []
 
-region = ()
-region = getWindowRegion()
-runTime = 0
+  region = ()
+  region = getWindowRegion()
+  runTime = 0
 
-fileName = './trainingDatas/trainingData-{}.npy'.format(START_VALUE)
-while os.path.isfile(fileName):
-  START_VALUE += 1
   fileName = './trainingDatas/trainingData-{}.npy'.format(START_VALUE)
-print('file from {}.'.format(START_VALUE))
-
-while True:
-  # benchmark Start
-  firstTime = time.time()
-  
-
-  img = grabScreen(region)
-  img = cv.resize(img, (480, 270))
-  trainingData.append(img)
-
-  if len(trainingData) % 100 == 0:
-    print(len(trainingData))
-  if len(trainingData) == 500:
-    np.save(fileName, trainingData)
+  while os.path.isfile(fileName):
     START_VALUE += 1
-    trainingData = []
-    print('Data has SAVED!')
     fileName = './trainingDatas/trainingData-{}.npy'.format(START_VALUE)
+  print('file from No.{}.'.format(START_VALUE))
 
-  # benchmark End
-  lastTime = time.time()
-  benchmark = lastTime - firstTime
-  runTime = runTime + benchmark
-  if runTime > 1:
-    fps = '{:.2f}'.format(turnToFPS(benchmark))
-    print(str(fps) + ' FPS')
-    runTime = 0
+  while True:
+    # benchmark Start
+    firstTime = time.time()
+
+    
+
+    img = grabScreen(region)
+    # 控制帧率
+    time.sleep(0.008)
+    # 修改下行使数据能够适应CNN
+    # img = cv.resize(img, (480, 270))
+    trainingData.append(img)
+
+    if len(trainingData) == 500:
+      np.save(fileName, trainingData)
+      START_VALUE += 1
+      trainingData = []
+      print('File {} has SAVED!'.format(START_VALUE))
+      fileName = './trainingDatas/trainingData-{}.npy'.format(START_VALUE)
+
+    # benchmark End
+    lastTime = time.time()
+    benchmark = lastTime - firstTime
+    runTime = runTime + benchmark
+    if runTime > 1:
+      fps = '{:.2f}'.format(turnToFPS(benchmark))
+      print(str(fps) + ' FPS')
+      runTime = 0
+
+def test():
+  region = ()
+  region = getWindowRegion()
+  runTime = 0
+
+  while True:
+    # benchmark Start
+    firstTime = time.time()
+    
+
+    img = grabScreen(region)
+    # img = cv.resize(img, (480, 270))
+    key = cv.waitKey(1)
+    if key == ord('q'):
+      break
+    else:
+      cv.imshow('show', img)
+
+
+    # benchmark End
+    lastTime = time.time()
+    benchmark = lastTime - firstTime
+    runTime += benchmark
+    if runTime > 1:
+      fps = '{:.2f}'.format(turnToFPS(benchmark))
+      print(str(fps) + ' FPS')
+      runTime = 0
+
+
+main()
+# test()
